6d262865bca206fe20411999e668dd5f74ef04bb
Connor Munger <connormunger@gmail.com>
2022-11-21 03:06:35 +0000

    Added service account, storage bucket, attached disk to main.tf. Added /var/www directory, created ext4 filesystem for data, mounted data to /var/www.

--- project2/main.tf
+++ project2/main.tf
@@ -53,6 +53,7 @@ resource "google_compute_firewall" "default_firewall" {
 resource "google_compute_instance" "vm_instance" {
   name         = "dokuwiki"
   machine_type = "e2-micro"
+  allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
@@ -65,10 +66,61 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
+
+  attached_disk {
+    source = google_compute_disk.data-disk.self_link
+    device_name = "data"
+  }
+
+  service_account {
+    email  = google_service_account.dokuwiki-service-account.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_disk" "data-disk" {
+  name  = "data"
+  type  = "pd-ssd"
+  size = "100"
+}
+
+#Service Account
+resource "google_service_account" "dokuwiki-service-account" {
+  account_id   = "dokuwiki-service-account"
+  display_name = "dokiwiki-service-account"
+  description = "Service account for dokuwiki"
+}
+
+resource "google_project_iam_member" "project_member" {
+  role = "roles/compute.viewer"
+  member = "serviceAccount:${google_service_account.dokuwiki-service-account.email}"
+}
+
+#Storage Bucket
+resource "google_storage_bucket" "dokuwiki-backup-bucket" {
+  name          = "dokuwiki-backup-storage-bucket-connor-cis91"
+  location      = "US-CENTRAL1"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+	age = 180
+    }
+    action {
+      type = "Delete"
+    }
+   } 
+}
+
+resource "google_storage_bucket_iam_binding" "binding" {
+  bucket = google_storage_bucket.dokuwiki-backup-bucket.name
+  role = "roles/storage.objectAdmin"
+  members = [
+    "serviceAccount:dokuwiki-service-account@cis91-361901.iam.gserviceaccount.com",
+  ]
 }
 
 # Output for SSH
-
 output "external-ip" {
   value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
