478de482844602843870f704a3c66cfe2b0da530
Connor Munger <connormunger@gmail.com>
2022-11-21 03:33:48 +0000

    adding backup.sh to github

---
+++ project2/backup.sh
@@ -0,0 +1,7 @@
+# Put this in /etc/cron.hourly/backup
+TARGET="gs://dokuwiki-backup-storage-bucket-connor-cis91"
+
+tar_file=/tmp/dokuwiki-backup-$(date +%s).tar.gz
+tar -czf $tar_file /var/www/html 2>/dev/null 
+/snap/bin/gsutil cp $tar_file $TARGET
+rm -f $tar_file
